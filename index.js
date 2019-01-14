d7a4c38bc99cb790da2c507c6171af70d3207ad8
billy <billyliadphillips@gmail.com>
2019-01-14 10:05:37 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Set up express app
+const app = express();
+
+
+
+// Listen for requests
+
+app.listen(process.env.port || 4000, () => {
+    console.log('now listening for requests');
+});
---
+++ routes/api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+// get a list of ninjas from the db
+router.get('/ninjas', (req, res) => {
+    res.send({type: 'GET'})
+});
+
+// add a new ninjas to db
+router.post('/ninjas', (req, res) => {
+    res.send({type: 'POST'})
+});
+
+// update ninja in db
+router.put('/ninjas/:id', (req, res) => {
+    res.send({type: 'PUT'})
+});
+
+// delete a ninja
+router.get('/ninjas/:id', (req, res) => {
+    res.send({type: 'DELETE'})
+});
+
